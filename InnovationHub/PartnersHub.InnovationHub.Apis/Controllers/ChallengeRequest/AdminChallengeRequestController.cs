63ccd32ba0c444b3379336d084698d465a73d92c
eyadhamdy-meem <eyad.hamdy@meemdev.com>
2025-11-03 12:39:40 +0200

    controller created

---
+++ InnovationHub/PartnersHub.InnovationHub.Apis/Controllers/ChallengeRequest/AdminChallengeRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartnersHub.InnovationHub.Apis.Controllers.Base;
+
+namespace PartnersHub.InnovationHub.Apis.Controllers.ChallengeRequest
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AdminChallengeRequestController : ApiBaseController<AdminChallengeRequestController>
+    {
+    }
+}
